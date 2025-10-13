d6a639c5e648399012d35d4cc3252743c6d201ba
Dominik Stypuła <dominik.stypula@seranking.com>
2025-10-13 19:36:48 +0200

    fix: header not needed in the test script

--- test-http-server-tools-list.sh
+++ test-http-server-tools-list.sh
@@ -3,7 +3,6 @@
 curl -X POST http://localhost:5000/mcp \
   -H "Content-Type: application/json" \
   -H "Accept: application/json, text/event-stream" \
-  -H "Authorization: Bearer $TOKEN" \
   -d '{
     "jsonrpc": "2.0",
     "id": "1",
