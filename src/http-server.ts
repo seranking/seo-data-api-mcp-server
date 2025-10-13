8a6be86a49d95582ee0354ca6c2bf32144d14db6
Dominik Stypuła <dominik.stypula@seranking.com>
2025-10-13 14:33:24 +0200

    feat: usage of the dotenv which allows to se the port and domain

--- src/http-server.ts
+++ src/http-server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from "express";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
